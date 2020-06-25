66557d2cb2f82db873865191a89064e852b4241b
Martin Hunt <martin.g.hunt@gmail.com>
2020-06-25 15:27:10 +0000

    Split normalise_vcf into separate commands

--- cluster_vcf_records/utils.py
+++ cluster_vcf_records/utils.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import subprocess
 import sys
 
@@ -26,9 +27,21 @@ def syscall(command):
 
 
 def normalise_vcf(vcf_in, ref_fasta, vcf_out):
-    command = f"vcfbreakmulti {vcf_in} | vcfallelicprimitives -L 10000 | vt normalize -r {ref_fasta} - | vcfuniq > {vcf_out}"
-    syscall(command)
+    # Would be nice to pipe all these to save disk IO. ie
+    # f"vcfbreakmulti {vcf_in} | vcfallelicprimitives -L 10000 | vt normalize -r {ref_fasta} - | vcfuniq > {vcf_out}"
+    # But am concerned about errors, where error code getting lost in pipes.
+    # So run one by one using temp files and then clean up.
+    vcf_breakmulti = f"{vcf_out}.1.breakmulti.vcf"
+    vcf_allelic = f"{vcf_out}.2.allelicprimitives.vcf"
+    vcf_normalize = f"{vcf_out}.3.normalize.vcf"
+    syscall(f"vcfbreakmulti {vcf_in} > {vcf_breakmulti}")
+    syscall(f"vcfallelicprimitives -L 10000 {vcf_breakmulti} > {vcf_allelic}")
+    syscall(f"vt normalize -r {ref_fasta} {vcf_allelic} > {vcf_normalize}")
+    syscall(f"vcfuniq {vcf_normalize} > {vcf_out}")
+    os.unlink(vcf_breakmulti)
+    os.unlink(vcf_allelic)
+    os.unlink(vcf_normalize)
 
 
 def rm_rf(filename):
-    syscall("rm -rf {filename}")
+    syscall(f"rm -rf {filename}")
