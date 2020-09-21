1d37e8fcc7c1813cc0c142901dde648429e70061
Martin Hunt <martin.g.hunt@gmail.com>
2020-09-21 00:56:36 +0000

    Version bump 0.13.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="cluster_vcf_records",
-    version="0.12.4",
+    version="0.13.0",
     description="Package to cluster VCF records. For use by gramtools and minos",
     packages=find_packages(),
     author="Martin Hunt",
